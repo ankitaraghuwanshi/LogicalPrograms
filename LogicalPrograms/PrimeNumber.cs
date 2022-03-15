431ff04e73a3b968c1b89691a9c2a3d79c5a1e9a
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-15 16:41:04 +0530

    [Ankita] Add : Program for Prime Number

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public void Prime()
+        {
+            int num;
+            Console.WriteLine("Enter the number : ");
+            num = int.Parse(Console.ReadLine());
+            if (num == 0 || num == 1)
+            {
+                Console.WriteLine(num + "is not a prime number");
+            }
+            else
+            {
+                for (int i = 2; i < num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        Console.WriteLine(num + " is not prime number");
+                        return;
+                    }
+                }
+                Console.WriteLine(num + " is a prime number");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,7 @@ namespace LogicalPrograms
         {
            
             Console.WriteLine("1 for FINDING fibonacii series");
+             Console.WriteLine("2 for FINDING prime number");
             Console.WriteLine("Enter the according to  above condition : \n");
 
             int answer = int.Parse(Console.ReadLine());
@@ -18,7 +19,11 @@ namespace LogicalPrograms
                     fibonacciSeries.Fb();
                     break;
 
-               
+                case 2:
+                    PrimeNumber primeNumber= new PrimeNumber();
+                    primeNumber.Prime();
+                    break;
+
 
                 default:
                     Console.WriteLine("Please enter a valid number.");
