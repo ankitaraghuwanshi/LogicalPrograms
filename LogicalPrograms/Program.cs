521afae344c0eadaf79cb101b9a290807bd34f4f
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-15 17:32:32 +0530

    [Ankita Add: program for Reverse number]

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -8,6 +8,7 @@ namespace LogicalPrograms
            
             Console.WriteLine("1 for FINDING fibonacii series");
              Console.WriteLine("2 for FINDING prime number");
+            Console.WriteLine("3 for FINDING reverse Number");
             Console.WriteLine("Enter the according to  above condition : \n");
 
             int answer = int.Parse(Console.ReadLine());
@@ -23,7 +24,10 @@ namespace LogicalPrograms
                     PrimeNumber primeNumber= new PrimeNumber();
                     primeNumber.Prime();
                     break;
-
+                case 3:
+                    ReverseNumber reverseNumber = new ReverseNumber();
+                    reverseNumber.Reverse();
+                    break;
 
                 default:
                     Console.WriteLine("Please enter a valid number.");
---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public void Reverse()
+        {
+            Console.WriteLine("enter the number for Reverse");
+            int num = int.Parse(Console.ReadLine());
+            int result = 0;
+            int rem;
+            while (num != 0)
+            {
+                rem = num % 10;
+                result = result * 10 + rem;
+                num = num / 10;
+            }
+            Console.WriteLine(result);
+        }
+
+    }
+}
